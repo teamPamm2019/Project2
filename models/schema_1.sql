10c5e09d2f9b231df776e913bf065088e48e9fff
Miguel Galarraga <miguelangelgalarraga@gmail.com>
2020-01-18 13:13:03 -0500

    just built the seed_cocktail file, and modified the schema_1 file

--- models/schema_1.sql
+++ models/schema_1.sql
@@ -9,9 +9,15 @@ CREATE TABLE drink_categories (
 
 
 CREATE TABLE Cocktail (
-    cocktail_id int NOT NULL,
+    cocktail_id int AUTO_INCREMENT,
     drink varchar(45) NOT NULL,
     cat_id int,
     PRIMARY KEY (cocktail_id),
     FOREIGN KEY (cat_id) REFERENCES drink_categories(cat_id)
 );
+
+INSERT INTO drink_categories (category) VALUES ("whiskey");
+INSERT INTO drink_categories (category)  VALUES ("tequila");
+INSERT INTO drink_categories (category)  VALUES ("vodka");
+INSERT INTO drink_categories (category)  VALUES ("rum");
+
---
+++ models/seed_cocktail.sql
@@ -0,0 +1,42 @@
+INSERT INTO cocktail (drink,cat_id) VALUES ("manhattan",1);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("cocktail Sour",1)
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("John Collins",1);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("Coffee Recipe",1);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("Mint Julep",1)
+
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("Tequila Sunrise",2);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("Classic Margarita",2)
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("Tequila Sour",2);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("Tequila Blood Orange",2);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("Tequila Corpse Reviver",2)
+
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("absolut_cosmopolitan",3);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("finlandia_vodka_wild_berritini",3)
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("absolut_elyx_vodka_martini_cocktail",3);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("ketel_one_vodka_martini",3);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("vodka_soda",3)
+
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("pina colada",4);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("dark n stormy",4)
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("daquiri",4);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("raspberry rum mule",4);
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("classic mojito",4)
