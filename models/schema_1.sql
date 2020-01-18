069094f1964248cc37921480322255478656f54e
Patrick Heron <hpa434@gmail.com>
2020-01-18 14:36:51 -0500

    added method and glass

--- models/schema_1.sql
+++ models/schema_1.sql
@@ -8,17 +8,39 @@ CREATE TABLE drink_categories (
 );
 
 CREATE TABLE Cocktail (
-    cocktail_id int AUTO_INCREMENT NOT NULL,
-    drink varchar(45) NOT NULL,
-    cat_id int,
-    PRIMARY KEY (cocktail_id),
-    FOREIGN KEY (cat_id) REFERENCES drink_categories(cat_id)
+  cocktail_id int AUTO_INCREMENT NOT NULL,
+  drink varchar(45) NOT NULL,
+  cat_id int,
+  PRIMARY KEY (cocktail_id),
+  FOREIGN KEY (cat_id) REFERENCES drink_categories(cat_id)
 );
 
 CREATE TABLE ingredients (
-    ingredients_id int AUTO_INCREMENT NOT NULL,
-    drink varchar(45) NOT NULL,
-    cocktail_id int,
-    PRIMARY KEY (ingredients_id),
-    FOREIGN KEY (cocktail_id) REFERENCES drink_categories(cat_id)
+  ingredients_id int AUTO_INCREMENT NOT NULL,
+  drink varchar(45) NOT NULL,
+  cocktail_id int,
+  PRIMARY KEY (ingredients_id),
+  FOREIGN KEY (cocktail_id) REFERENCES drink_categories(cat_id)
 );
+
+CREATE TABLE method (
+  method_id int AUTO_INCREMENT NOT NULL,
+  step varchar(45) NOT NULL,
+  cocktail_id int,
+  PRIMARY KEY (method_id),
+  FOREIGN KEY (cocktail_id) REFERENCES Cocktail(cocktail_id)
+);
+
+-- Glass table for later
+CREATE TABLE glass (
+  glass_id INT AUTO_INCREMENT NOT NULL,
+  glass varchar(45) NOT NULL,
+  drink_id int,
+  PRIMARY KEY (glass_id),
+  FOREIGN KEY (drink_id) REFERENCES Cocktail(cocktail_id)
+);
+
+INSERT INTO drink_categories (category) VALUES ("whiskey");
+INSERT INTO drink_categories (category)  VALUES ("tequila");
+INSERT INTO drink_categories (category)  VALUES ("vodka");
+INSERT INTO drink_categories (category)  VALUES ("rum");
