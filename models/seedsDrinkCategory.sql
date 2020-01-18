c275a77a6d1d1f2fbfeae8a4837f52ccc9505360
Aimee Bojorquez <abojo079@me.com>
2020-01-18 11:18:37 -0500

    server.js

---
+++ models/seedsDrinkCategory.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE ;
+USE ;
+-- Create the table actors.
+CREATE TABLE drink_categories (
+  id int AUTO_INCREMENT,
+  category varchar(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+-- Insert a set of records.
+INSERT INTO drink_categories (category) VALUES ("whiskey");
+INSERT INTO drink_categories (category)  VALUES ("tequila");
+INSERT INTO drink_categories (category)  VALUES ("vodka");
+INSERT INTO drink_categories (category)  VALUES ("rum");
