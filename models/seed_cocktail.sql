ac98959de5b6106594ce6dfab344ac783d883eec
Miguel Fernandez <miguelfernandez023@gmail.com>
2020-01-20 14:06:15 -0500

    removed image links in seed_cocktail

--- models/seed_cocktail.sql
+++ models/seed_cocktail.sql
@@ -32,12 +32,11 @@ INSERT INTO cocktail (drink,cat_id) VALUES ("vodka_soda",3)
 
 
 INSERT INTO cocktail (drink,cat_id) VALUES ("pina colada",4);
-https://static.vinepair.com/wp-content/uploads/2016/04/pina-colada-card.jpg
+
 INSERT INTO cocktail (drink,cat_id) VALUES ("dark n stormy",4)
-https://static.vinepair.com/wp-content/uploads/2018/03/Dark-N-Stormy-Recipe-card.jpg
+
 INSERT INTO cocktail (drink,cat_id) VALUES ("daquiri",4);
-https://static.vinepair.com/wp-content/uploads/2016/03/daiquiri-card.jpg
+
 INSERT INTO cocktail (drink,cat_id) VALUES ("raspberry rum mule",4);
-https://www.palmbay.com/sites/default/files/styles/blog_recipe/public/2019-08/DrySackCobbler.jpg
-INSERT INTO cocktail (drink,cat_id) VALUES ("classic mojito",4)
-https://static.vinepair.com/wp-content/uploads/2018/03/Mojito-Recipe-card.jpg
+
+INSERT INTO cocktail (drink,cat_id) VALUES ("classic mojito",4);
