3ea923dd9c70934c58b879e2e82949aabf05ae00
Miguel Fernandez <miguelfernandez023@gmail.com>
2020-01-20 13:25:30 -0500

    updated rum seeds. added links to rum images in seeds_cocktails

--- models/seedsRum.sql
+++ models/seedsRum.sql
@@ -16,19 +16,3 @@ INSERT INTO rum (ingredients)
 VALUES ("2 lime wedges, 2 teaspoons sugar, 3/4 to 1 cup ice cubes, 2 mint sprigs, 2 ounces light rum, 
 1/2 cup club soda, chilled, GARNISH: Mint sprig and lime slice")
 
-INSERT INTO rum (ingredients)
-VALUES ("1-1/2 to 2 cups ice cubes, 2 ounces light rum, 3/4 ounce Triple Sec, 1/2 ounce lemon juice, 1-1/2 teaspoons lime juice, 
-1-1/2 teaspoons amaretto. GARNISH: Lime slice and twist")
-
-INSERT INTO rum (ingredients)
-VALUES ("1 oz Rum, 3 oz Coca-Cola, Garnish: Lime wedge, Glass: Highball")
-
-INSERT INTO rum (ingredients)
-VALUES ("1⁄2 tsp Ground cinnamon, 1⁄2 cup sugar, 1 Lemon wedge, 1 1⁄2 oz Spiced rum, 3⁄4 oz Marie Brizard Orange Curaçao, 1⁄2 oz Simple syrup (one part sugar, one part water) 1 oz Lemon juice")
-
-INSERT INTO rum (ingredients)
-VALUES ("Beer, 3⁄4 oz Amaretto, 1⁄4 oz Rum")
-
-INSERT INTO rum (ingredients)
-VALUE ("2 oz. light rum, 2 oz. dark rum, 2 oz. passion fruit juice, 1 oz. orange juice, 1/2 oz. fresh lime juice, 
-1 tablespoon simple syrup, 1 tablespoon grenadine. Orange slice and cherry, for garnish")
