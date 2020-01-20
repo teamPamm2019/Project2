f5e8f35b94a61a41284feabcec7d2c1aa5ca453d
Aimee Bojorquez <abojo079@me.com>
2020-01-20 16:18:54 -0500

    server.js

--- models/db/seed Tequila.sql
+++ models/db/seed Tequila.sql
@@ -1,93 +1,72 @@
-INSERT INTO tequila (glass, 
-ingredients,
- method)
+INSERT INTO ingredients( 
+ingredients)
 
- VALUES ("Tequila Sunrise", 
+ VALUES (
  "2 ounces Blanco tequila,
 4 ounces Fresh orange juice,
-1⁄4 ounces Grenadine", 
- "Add the tequila and then the orange juice to a chilled highball glass.
-Float the grenadine on top.
-Garnish with an orange slice and a cherry.");
+1⁄4 ounces Grenadine");
 
-INSERT INTO tequila (glass, ingredients, method)
+INSERT INTO ingredients (ingredients)
  VALUES ("Classic Margarita",
  "2 ounces Blanco tequila,
 1 ounces Fresh lime juice,
 1⁄2 ounces Orange liqueur,
 1⁄2 ounces Agave syrup,
-Garnish: lime wheel and kosher salt", 
- "Add all the ingredients into a shaker with ice, and shake until chilled.
-Strain into the prepared rocks glass over fresh ice.
-Garnish with a lime wheel and kosher salt (optional).");
+Garnish: lime wheel and kosher salt");
 
-INSERT INTO tequila (glass, ingredients, method) 
-VALUES ("Tequila Sour",
+INSERT INTO ingredients (ingredients) 
+VALUES (
  "1 1⁄2 oz Tequila Reposado,
 3⁄4 oz Fresh lemon juice,
 1⁄4 oz Fresh lime juice,
 3⁄4 oz Light agave nectar,
-1  Egg white", 
- "Add all the ingredients except the bitters to a shaker, and dry-shake (without ice) for 15 seconds.
-Add ice, shake, and double-strain into a rocks glass.");
+1  Egg white");
 
-INSERT INTO tequila (glass, ingredients, method)
- VALUES ("Tequila Blood Orange",
+INSERT INTO ingredients (ingredients)
+ VALUES (
  "1 1⁄2 oz Cazadores blanco tequila, 
 1 oz Blood orange juice,
 1⁄2 oz Fresh lime juice,
 1⁄2 oz Agave nectar,
-Club soda", 
- "Combine the tequila, blood orange and lime juices, and agave nectar in a shaker with ice, and shake.
-Strain into a Collins or Highball glass filled with ice, and top with the club soda.
-Garnish with a mint sprig.");
+Club soda");
 
-INSERT INTO tequila (glass, ingredients, method)
+INSERT INTO ingredients (ingredients)
  VALUES
- ("Tequila Corpse Reviver",
+ (
  "3⁄4 oz Cazadores blanco tequila, 
 3⁄4 oz sweet blanc vermouth,
 3⁄4 oz Lime juice,
 1⁄2 oz Triple sec,
 1⁄4 oz Orange juice,
 1 tsp Absinthe", 
- "Combine all ingredients in cocktail shaker filled with ice.
-Shake and double strain into a coupe glass.
-Garnish with an orange peel.");
+ );
 
-INSERT INTO tequila (glass, ingredients, method) 
-VALUES ("Tequila Martini", 
+INSERT INTO ingredients (ingredients) 
+VALUES (
 "1 1⁄2 oz Blanco tequila,
 3⁄4 oz sweet white vermouth,
 1⁄4 oz Lemon juice,
 2 dashes Orange bitters.
-Garnish: lemon twist", 
-"Combine all ingredients in a cocktail shaker filled with ice.
-Shake and double strain into a martini glass.
-Garnish with lemon twist.");
+Garnish: lemon twist" 
+);
 
-INSERT INTO tequila (glass, ingredients, method)
- VALUES ("Tequila Limeade",
+INSERT INTO ingredients (ingredients)
+ VALUES (
  "1⁄2 oz Light agave nectar,
 8 sprigs Cilantro leaves, plus a full sprig for garnish,
 2 slices Fresh jalapeño,
 1 1⁄2 oz Cazadores tequila blanco,
 3⁄4 oz Fresh lime juice,
 12 oz Club soda
-Garnish: cilantro sprig",
- "Combine agave nectar, cilantro leaves and jalapeño slices in a shaking tin.
-Muddle gently.Add tequila, lime juice, and ice.
-Shake and double strain into a collins or highball glass filled with ice.
-Top with club soda.
-Garnish with fresh sprig of cilantro.");
+Garnish: cilantro sprig"
+ );
 
-INSERT INTO tequila (glass, ingredients, method) 
-VALUES ("Corzo Tequila Toast", 
+INSERT INTO tequila (ingredients) 
+VALUES ( 
 "1 oz Corzo Silver Tequila
 1⁄2 oz Vanilla cognac Champagne. 
-Garnish: 1 raspberry", 
- "Add the tequila and cognac to a chilled Champagne flute and fill with Champagne.
-Garnish with raspberries.");
+Garnish: 1 raspberry"
+ );
 
 INSERT INTO tequila (glass, ingredients, method) 
 VALUES ("Herradura Tequila Mojito",
---
+++ models/db/seedsIngredientsMaster.sql
@@ -0,0 +1,67 @@
+USE speakez;
+
+
+
+INSERT INTO ingredients(drink, cocktail_id)VALUES ("2 ounces rye whiskey, bourbon, or Canadian whiskey, 1 ounce sweet vermouth, 2 to 3 dashes Angostura Bitters, Garnish: cherry", 1);
+
+
+
+INSERT INTO ingredients(drink, cocktail_id) VALUES ("1 1/2 ounces ​whiskey, 1 1/2 ounces ​lemon juice (fresh), 3/4 ounce ​simple syrup Optional: ​egg white, Garnish: ​​maraschino cherry or lemon peel", 3);
+
+
+
+INSERT INTO ingredients(drink, cocktail_id) VALUES ("2 ounces Blanco tequila,
+4 ounces Fresh orange juice,
+1⁄4 ounces Grenadine", 7);
+
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("2 ounces Blanco tequila,
+1 ounces Fresh lime juice,
+1⁄2 ounces Orange liqueur,
+1⁄2 ounces Agave syrup,
+Garnish: lime wheel and kosher salt", 8);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("1 1⁄2 oz Tequila Reposado,
+3⁄4 oz Fresh lemon juice,
+1⁄4 oz Fresh lime juice,
+3⁄4 oz Light agave nectar,
+1  Egg white", 9);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("1 1⁄2 oz Cazadores blanco tequila, 
+1 oz Blood orange juice,
+1⁄2 oz Fresh lime juice,
+1⁄2 oz Agave nectar,
+Club soda", 10);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("3⁄4 oz Cazadores blanco tequila, 
+3⁄4 oz sweet blanc vermouth,
+3⁄4 oz Lime juice,
+1⁄2 oz Triple sec,drinkredients
+1⁄4 oz Orange juice,
+1 tsp Absinthe",11);
+
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("1/2 oz Absolut Citron vodka, 2/3 oz Cranberry juice, 1/2 oz Triple sec liqueur, 1/3 oz Fresh lime juice",12);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("3/4 oz Finlandia Wild Berries Fusion Vodka, 1/4 oz Finlandia Vodka, 1/4 oz Chambord",13);
+
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("2 1/2 oz Absolut Elyx vodka, 1/2 oz Lillet Blanc apéritif",14);
+
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("1 1/2 oz Ketel One Vodka, 1/2 oz Dry vermouth",15);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("2 oz vodka, 2 oz club soda, 1 squeeze",16);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("2 oz White Rum, 2 oz Coconut Cream, 2 oz Pineapple Juice",17);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("2 oz Dark rum as needed Gdrinker beer",18);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("2 oz White rum, ¾ oz Lime, ¾ oz Simple syrup",19);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("2 ounces of rum, ½, lime juiced, 1 teaspoon agave nectar, 5 raspberries, ½ bottle of gdrinker beer, 
+½ cup of ice, Garnish: Raspberries and rosemary",20);
+
+INSERT INTO ingredients (drink, cocktail_id) VALUES ("2 lime wedges, 2 teaspoons sugar, 3/4 to 1 cup ice cubes, 2 mint sprigs, 2 ounces light rum, 
+1/2 cup club soda, chilled, GARNISH: Mint sprig and lime slice",21);
+
