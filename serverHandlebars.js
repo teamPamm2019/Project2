eacea046f34ae55f1f3e7558157daa143930fbfc
Miguel Fernandez <Miguel@wood-bronstein.local>
2020-01-23 16:33:18 -0500

    added serverHandlebar.js

---
+++ serverHandlebars.js
@@ -0,0 +1,19 @@
+
+
+var express = require("express");
+var app = express();
+var exphbs = require("express-handlebars");
+
+// Handlebars
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('example');
+});
+
+
+app.listen(8080, () => {
+  console.log('Server is listening on port 🌎', 8080);
+});
