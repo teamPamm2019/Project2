2cbcc3b9e2910c818f7bdbdcfbf1d7214ce79907
Miguel Galarraga <miguelangelgalarraga@gmail.com>
2020-01-23 22:21:30 -0500

    add the sql files to test the app so far

---
+++ db/Categories.sql
@@ -0,0 +1,6 @@
+USE speakEZ;
+
+INSERT INTO drinkcategories (category) VALUES ("whiskey");
+INSERT INTO drinkcategories (category)  VALUES ("tequila");
+INSERT INTO drinkcategories (category)  VALUES ("vodka");
+INSERT INTO drinkcategories (category)  VALUES ("rum");
---
+++ db/Cocktail&images.sql
@@ -0,0 +1,12 @@
+
+USE SpeakEZ;
+
+INSERT INTO cocktails(drink,images,drinkcategories_id) VALUES ("Manhattan","../db/images/Manhattan.png",1);
+
+INSERT INTO cocktails (drink,images,drinkcategories_id) VALUES ("cocktails Sour","../db/images/cocktailsSour.png",1);
+
+INSERT INTO cocktails (drink,images,drinkcategories_id) VALUES ("John Collins","../db/images/JohnCollins.png",1);
+
+INSERT INTO cocktails (drink,images,drinkcategories_id) VALUES ("Coffee Recipe","../db/images/CoffeeRecipe.png",1);
+
+INSERT INTO cocktails (drink,images, drinkcategories_id) VALUES ("Mint Julep","../db/images/MintJulep.png",1);
