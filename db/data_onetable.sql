67dce99963c04e1b377f44f740a10c4dc7ea13f7
Miguel Galarraga <miguelangelgalarraga@gmail.com>
2020-01-27 23:21:56 -0500

    all files ready to be posted in gibhub before deplying the app in heroku

--- db/data_onetable.sql
+++ db/data_onetable.sql
@@ -9,3 +9,41 @@ INSERT INTO bars (category,drink,image,ingredients,method) VALUES ("whiskey","jo
 INSERT INTO bars(category,drink,image,ingredients,method) VALUES ("whiskey", "Coffee Reciper","/images/CoffeeRecipe.png","4 ounces coffee (strong, rich; brewed, hot), 1 1/2 ounce ​Irish whiskey, 2 teaspoons ​brown sugar, 1 ounce heavy cream (lightly whipped","Gather the ingredients, Place the brown sugar into a warm Irish coffee glass, mug, or other heat-proof glass, Add the coffee and stir until dissolved, Add the Irish whiskey and stir again, Float the lightly whipped heavy cream on top by pouring it over the back of a spoon, Do not stir again. Instead, drink the coffee through the cream");
 
 INSERT INTO bars(category,drink,image,ingredients,method) VALUES ("whiskey", "mint julep","/images/MintJulep.png","4 to 5 mint sprigs (leaves only), 2 sugar cubes (or 1/2 ounce simple syrup), 2 1/2 ounces bourbon whiskey, Garnish: mint sprig","Gather the ingredients, Place the mint leaves and sugar or simple syrup into a julep cup, collins glass, or double old-fashioned glass Muddle well to dissolve the sugar and release the oil and aroma of the mint, Add the bourbon Fill with crushed ice and stir well until the glass becomes frosty, Garnish with a mint sprig");
+
+INSERT INTO bars(category,drink,image,ingredients,method) VALUES("tequila","Tequila Sunrise","/images/tequilasunrise.jpeg","2 ounces Blanco tequila,
+4 ounces Fresh orange juice,
+1⁄4 ounces Grenadine","Add the tequila and then the orange juice to a chilled highball glass.
+Float the grenadine on top
+Garnish with an orange slice and a cherry.");
+
+INSERT INTO bars(category,drink,image,ingredients,method) VALUES("tequila","Classic Margarita","/images/classicmargarita.jpeg","2 ounces Blanco tequila,
+1 ounces Fresh lime juice,
+1⁄2 ounces Orange liqueur,
+1⁄2 ounces Agave syrup,
+Garnish: lime wheel and kosher salt","Add all the ingredients into a shaker with ice, and shake until chilled.
+Strain into the prepared rocks glass over fresh ice.
+Garnish with a lime wheel and kosher salt (optional).");
+
+INSERT INTO bars(category,drink,image,ingredients,method) VALUES("tequila","Tequila Sour","/images/tequilasour.jpeg","1 1⁄2 oz Tequila Reposado,
+3⁄4 oz Fresh lemon juice,
+1⁄4 oz Fresh lime juice,
+3⁄4 oz Light agave nectar,
+1  Egg white","Add all the ingredients except the bitters to a shaker, and dry-shake (without ice) for 15 seconds.
+Add ice, shake, and double-strain into a rocks glass.");
+
+INSERT INTO bars(category,drink,image,ingredients,method) VALUES("tequila","Tequila Blood Orange","/images/tequilabloodorange.jpeg","1 1⁄2 oz Cazadores blanco tequila, 
+1 oz Blood orange juice,
+1⁄2 oz Fresh lime juice,
+1⁄2 oz Agave nectar,
+Club soda","Combine the tequila, blood orange and lime juices, and agave nectar in a shaker with ice, and shake.
+Strain into a Collins or Highball glass filled with ice, and top with the club soda.
+Garnish with a mint sprig.");
+
+INSERT INTO bars(category,drink,image,ingredients,method) VALUES("tequila","Tequila Corpse Reviver","/images/tequilacorpsereviver.jpeg","3⁄4 oz Cazadores blanco tequila, 
+3⁄4 oz sweet blanc vermouth,
+3⁄4 oz Lime juice,
+1⁄2 oz Triple sec,drinkredients
+1⁄4 oz Orange juice,
+1 tsp Absinthe","Combine all ingredients in cocktail shaker filled with ice.
+Shake and double strain into a coupe glass.
+Garnish with an orange peel.");
